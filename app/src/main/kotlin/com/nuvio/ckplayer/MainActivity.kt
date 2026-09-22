79f5f28a57dac7d8b017393af0eb222efbc2c1b5
retrocodes12 <88876293+retrocodes12@users.noreply.github.com>
2026-09-23 05:09:36 +0530

    Import the semantics contentDescription the player's tap layer now sets

--- app/src/main/kotlin/com/nuvio/ckplayer/MainActivity.kt
+++ app/src/main/kotlin/com/nuvio/ckplayer/MainActivity.kt
@@ -195,6 +195,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.semantics.onClick as a11yClick
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
